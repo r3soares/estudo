fc98b46549ec31faabea336b42edd8a649ac11d6
Ricardo M B Soares <r3soares@gmail.com>
2022-03-03 20:46:44 -0300

    Incluido Projeto para pratica com HTML mais servidor asp net

---
+++ Computação/Pratica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pratica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
